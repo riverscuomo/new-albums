0a247909f480bf17e4e0aa01a03ad311044429e2
donramone <luigi_lipreri@hotmail.com>
2022-03-26 02:54:00 -0300

    Fix update slow

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import random
 import config
 from services.get_spotify import get_spotify
 from rich import print
-
+from time import time
 import json
 
 
@@ -80,6 +80,7 @@ def get_spotify_songs_from_playlist(
 
 
 def get_new_album_ids(limit=50):
+    print("Get new albums...")
     """
     Get all the album ids from the last x new albums. It doesn't include single-only releases OR any genres you've marked as reject.
     """
@@ -107,38 +108,39 @@ def remove_reject_genres(new_albums):
     with open("reject_genres.json") as f:
         reject_genres = json.load(f)
     albums = []
-    print_rejected = []
-    print_update = []
+    rejected_print = []
+    saved_print = []
     
     for album in new_albums:
         main_artist = album["artists"][0]
         artist_name = main_artist["name"]
         main_artist = spotify.artist(main_artist["id"])
         if( main_artist["genres"] == []):
-            artist_genres="unknow"  
+            artist_genres=["unknow"]  
         else:
             artist_genres = main_artist["genres"]
 
         if contains_reject_genre(reject_genres, artist_name, artist_genres):
-         print_rejected.append(f"- {artist_genres[0]} | {artist_name}")
+         rejected_print.append(f"- {artist_genres[0]} | {artist_name}")
          continue
         else:
-         print_update.append(f"- {artist_genres[0]} | {artist_name}")
+         saved_print.append(f"- {artist_genres[0]} | {artist_name}")
          albums.append(album)
-    print("*** Reject albums ***") 
-    print(*print_rejected, sep="\n") 
-    print("*** Saved in playlist ***")    
-    print(*print_update, sep="\n") 
+    print("****** Reject albums ******") 
+    print(*rejected_print, sep="\n") 
+    print("****** Saved albums ******")    
+    print(*saved_print, sep="\n") 
     return albums
 
 
 def contains_reject_genre(reject_genres, artist_name, artist_genres):
-    try:
-        artist_genre = artist_genres[0]
-    except:
+    artist_genre = artist_genres[0]
+    #try:
+        #artist_genre = artist_genres[0]
+    #except:
         # in case the artist has no genres
         #print(f"- [] | {artist_name} ")
-        return False
+        #return False
 
     for reject_genre in reject_genres:
         if (
@@ -155,24 +157,39 @@ def get_track_ids_for_album(album_id):
     return [x["id"] for x in album["tracks"]["items"]]
 
 def update_playlist(tracks):
-    print("updating spotify playlist")
+    print("updating spotify playlist...")
     result = spotify.user_playlist_replace_tracks(
         config.SPOTIFY_USER, config.PLAYLIST_ID, []
     )
     for track in tracks:
     	result = spotify.user_playlist_add_tracks(
-            config.SPOTIFY_USER, config.PLAYLIST_ID,tracks=[track]
+            config.SPOTIFY_USER, config.PLAYLIST_ID,track
+            #tracks=[track]
     )
-  
+    
+def split_track(track_ids):
+    #split track_ids into lists of size 100
+    track_id_lists = []
+    for i in range(0, len(track_ids), 100):
+        track_id_lists.append(track_ids[i : i + 100])
+    return track_id_lists
+    
 def main():
+    start_time = time()
+    
     new_album_ids = get_new_album_ids()
+    
     track_ids = []
+    #this for should not be in the main function
     for album_id in new_album_ids:
         track_ids.extend(get_track_ids_for_album(album_id))
-    update_playlist(track_ids)
-    print("done!")
+    
+    track_id_lists = split_track(track_ids)
+    update_playlist(track_id_lists)
 
- 
+    elapsed_time = time() - start_time
+    print("Done in: %0.10f seconds." % elapsed_time)
+     
 
 if __name__ == "__main__":
 

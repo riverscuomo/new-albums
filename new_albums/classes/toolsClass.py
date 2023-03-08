40d518272b78e06449e4b157c8c1a15a131445ed
Rivers Cuomo <riverscuomo@users.noreply.github.com>
2023-03-08 09:12:59 -0800

    if list order isn't important, list(set(my_list))

--- new_albums/classes/toolsClass.py
+++ new_albums/classes/toolsClass.py
@@ -6,13 +6,17 @@ class toolsClass :
         This function remove duplicates in an array.
         """
 
-        # Intilize a null list
-        unique_list_of_elements = []
+        # # Intilize a null list
+        # unique_list_of_elements = []
 
-        # Traverse for all elements
-        for x in list_of_elements:
-            # Csheck if exists in unique_list or not
-            if x not in unique_list_of_elements:
-                unique_list_of_elements.append(x)
-        return unique_list_of_elements
+        # # Traverse for all elements
+        # for x in list_of_elements:
+        #     # Csheck if exists in unique_list or not
+        #     if x not in unique_list_of_elements:
+        #         unique_list_of_elements.append(x)
+        # return unique_list_of_elements
+
+        # if list order isn't important, list(set(my_list)) 
+        # is much more fast and concise (less room for errors!).
+        return list(set(list_of_elements))
 
